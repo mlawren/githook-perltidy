4435513c8b2e6b74435e0a1284027f06f20d8fcf
Mark Lawrence <nomad@null.net>
2012-05-02 12:56:26 +1200

    Add empty App::githook_perltidy to allow CPAN indexing

---
+++ lib/App/githook_perltidy.pm
@@ -0,0 +1,30 @@
+package App::githook_perltidy;
+1;
+
+__END__
+
+=head1 NAME
+
+App::githook_perltidy - run perltidy and podtidy before Git commits
+
+=head1 DESCRIPTION
+
+This module exists purely to allow CPAN indexing.
+
+=head1 SEE ALSO
+
+L<githook-perltidy>(1)
+
+=head1 AUTHOR
+
+Mark Lawrence E<lt>nomad@null.netE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011-2012 Mark Lawrence <nomad@null.net>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+

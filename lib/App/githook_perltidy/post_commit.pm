e4a45ca31fdc5142cb8dc1eeb223c82a70d69e7a
Mark Lawrence <nomad@null.net>
2018-06-27 20:29:41 +0800

    Set version to 0.11.5

--- lib/App/githook_perltidy/post_commit.pm
+++ lib/App/githook_perltidy/post_commit.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent 'App::githook_perltidy';
 
-our $VERSION = '0.11.5_3';
+our $VERSION = '0.11.5';
 
 sub run {
 
@@ -19,7 +19,7 @@ App::githook_perltidy::post_commit - git post-commit hook
 
 =head1 VERSION
 
-0.11.5_3 (yyyy-mm-dd)
+0.11.5 (yyyy-mm-dd)
 
 =head1 DESCRIPTION
 
--- lib/App/githook_perltidy/pre_commit.pm
+++ lib/App/githook_perltidy/pre_commit.pm
@@ -7,7 +7,7 @@ use Path::Tiny;
 use Perl::Tidy;
 use Pod::Tidy;
 
-our $VERSION = '0.11.5_3';
+our $VERSION = '0.11.5';
 
 my $temp_dir;
 
@@ -197,7 +197,7 @@ App::githook_perltidy::pre_commit - git pre-commit hook
 
 =head1 VERSION
 
-0.11.5_3 (yyyy-mm-dd)
+0.11.5 (yyyy-mm-dd)
 
 =head1 SEE ALSO
 

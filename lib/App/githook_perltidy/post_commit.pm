813d3a66a8860dbe7ec2666790b39c109f3ad1d6
Mark Lawrence <nomad@null.net>
2018-07-13 11:29:13 +0800

    Start development on version 0.11.7_2

--- lib/App/githook_perltidy/post_commit.pm
+++ lib/App/githook_perltidy/post_commit.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent 'App::githook_perltidy';
 
-our $VERSION = '0.11.7_1';
+our $VERSION = '0.11.7_2';
 
 sub run {
 
@@ -19,7 +19,7 @@ App::githook_perltidy::post_commit - git post-commit hook
 
 =head1 VERSION
 
-0.11.7_1 (2018-07-13)
+0.11.7_2 (yyyy-mm-dd)
 
 =head1 DESCRIPTION
 

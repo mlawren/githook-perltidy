a01fd08f239a9e358a63d8f1ac32d75349811dae
Mark Lawrence <nomad@null.net>
2018-07-14 16:19:36 +0800

    0.11.10 (2018-07-14)
        Packaging:
            - Return to the original distribution name of
              App-githook-perltidy with a package name of
              App::githook_perltidy. Seems that I can't change anything
              without losing PAUSE history or permissions.

--- lib/App/githook_perltidy/install.pm
+++ lib/App/githook_perltidy/install.pm
@@ -5,7 +5,7 @@ use parent 'App::githook_perltidy';
 use File::Basename;
 use Path::Tiny;
 
-our $VERSION = '0.11.9';
+our $VERSION = '0.11.10';
 
 sub run {
     my $self = shift;
@@ -38,7 +38,7 @@ App::githook_perltidy::install - install git hooks
 
 =head1 VERSION
 
-0.11.9 (2018-07-13)
+0.11.10 (2018-07-14)
 
 =head1 SEE ALSO
 
--- lib/App/githook_perltidy/post_commit.pm
+++ lib/App/githook_perltidy/post_commit.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use parent 'App::githook_perltidy';
 
-our $VERSION = '0.11.9';
+our $VERSION = '0.11.10';
 
 sub run {
 
@@ -19,7 +19,7 @@ App::githook_perltidy::post_commit - git post-commit hook
 
 =head1 VERSION
 
-0.11.9 (2018-07-13)
+0.11.10 (2018-07-14)
 
 =head1 DESCRIPTION
 
